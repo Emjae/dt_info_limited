1240b67b36398472e6acfc45b0353b4f033a7ef0
Emjae <hrokolo434@gmail.com>
2018-07-30 19:55:53 -0400

    Initial commit

---
+++ dt.js
@@ -0,0 +1,31 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+	
+	ext.get_rate = {
+		$.ajax({
+              url: 'https://s3.amazonaws.com/dolartoday/data.json',
+              dataType: 'jsonp',
+              success: function( dolartoday ) {
+                  rate = dolartoday['USD']['dolartoday'];
+                  callback(rate);
+              }
+        });
+	}
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+		['R', 'current exchange rate', 'get_rate'],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('DolarToday extension', descriptor, ext);
+})({});
