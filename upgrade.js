bdef20fca872224293d6bb25a09731b19365c57f
Emjae <hrokolo434@gmail.com>
2018-12-13 21:15:32 -0500

    Update upgrade.js

--- upgrade.js
+++ upgrade.js
@@ -1,5 +1,5 @@
 (function(ext) {
-	var data = {"USD":{"dolartoday":0,"localbitcoin_ref":0,"efectivo_cucuta":0},"EUR":{"dolartoday":0,"sicad1":0,"efectivo_cucuta":0},"COL":{"efectivo":0},"EURUSD":{"rate":0}};
+	var data = { "_id": { }, "low": 0, "high": 0, "open": 0, "close": 0 };
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -9,40 +9,25 @@
         return {status: 2, msg: 'Ready'};
     };
 	
-	ext.get_data = function (){fetch("https://s3.amazonaws.com/dolartoday/data.json").then(function (j){return j.json()}).then(function (j){data = j})};
-	ext.dolartoday = function(type)
+	ext.get_data = function (){fetch("https://freedolar.com/getDailyOHLC?ccy=usdves").then(function (j){return j.json()}).then(function (j){data = j})};
+	ext.price = function(type)
 	{
-	if(type === "dollar"){return data.USD.dolartoday};
-	if(type === "euro"){return data.EUR.dolartoday};
-	};
-	ext.cash = function(type)
-	{
-	if(type === "dollar"){return data.USD.efectivo_cucuta};
-	if(type === "euro"){return data.EUR.efectivo_cucuta};
-	};
-	ext.bitcoin = function(type)
-	{
-	if(type === "dollar"){return data.USD.localbitcoin_ref};
-	if(type === "euro"){return data.USD.localbitcoin_ref*data.EURUSD.rate};
-	};
-	ext.eurusd = function(type)
-	{
-	return data.EURUSD.rate;
+	if(type === "open"){return data.open};
+	if(type === "high"){return data.high};
+	if(type === "low"){return data.low};
+	if(type === "close"){return data.close};
 	};
 	
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
 		[' ', 'get exchange rate data', 'get_data'],
-		['r', 'dolartoday %m.currency price', 'dolartoday' , 'dollar' ],
-		['r', '%m.currency cash price', 'cash', 'dollar' ],
-		['r', 'estimated %m.currency price through LocalBitCoins', 'bitcoin','dollar' ],
-		['r', 'EUR/USD rate', 'eurusd' ],
+		['r', '%m.type value', 'price' , 'close' ],
 		
         ],
-		menus: {currency: ["dollar","euro"]},
+		menus: {type: ["open","high","low","close"]},
     };
 
     // Register the extension
-    ScratchExtensions.register('DolarToday extension', descriptor, ext);
+    ScratchExtensions.register('Dolar paralelo extension', descriptor, ext);
 })({});
