71944f4a489b354adf9396e26ac62d732a312d3f
Emjae <hrokolo434@gmail.com>
2018-12-13 22:06:50 -0500

    Update upgrade.js

--- upgrade.js
+++ upgrade.js
@@ -1,5 +1,6 @@
 (function(ext) {
 	var data = [{"_id":{},"low":"0","high":"0","open":"0","close":"0"}];
+	var session = {"_id":"0","bid":"0","ask":"0","mid":"0","date":"0","high":"0","low":"0","open":"0","close":"0"};
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -10,6 +11,7 @@
     };
 	
 	ext.get_data = function (){fetch("https://cors.io/?https://freedolar.com/getDailyOHLC?ccy=usdves").then(function (j){return j.json()}).then(function (j){data = j})};
+	ext.get_session = function (){fetch("https://cors.io/?https://freedolar.com/getDay?ccy=usdves").then(function (j){return j.json()}).then(function (j){data = j})};
 	ext.price = function(type)
 	{
 	if(type === "open"){return data[0].open};
@@ -17,12 +19,17 @@
 	if(type === "low"){return data[0].low};
 	if(type === "close"){return data[0].close};
 	};
+	ext.sessionData = function(type)
+	{
+	return session.close
+	};
 	
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
 		[' ', 'get exchange rate data', 'get_data'],
 		['r', '%m.type value', 'price' , 'close' ],
+		['r', 'previous close', 'sessionData'],
 		
         ],
 		menus: {type: ["open","high","low","close"]},
